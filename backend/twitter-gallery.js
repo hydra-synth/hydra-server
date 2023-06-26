9b79b31275f6288f31eca7d8e0368a8a9c75efc2
ojack <ojackson@gmail.com>
2023-06-26 14:28:01 +0200

    removed submodules

--- backend/twitter-gallery.js
+++ backend/twitter-gallery.js
@@ -7,96 +7,97 @@ var tweet
 module.exports = (app) => {
   // server-side code related to saving and storing images
   // if has twitter key, enamble uploading
-  if(process.env.CONSUMER_KEY) {
+  if (process.env.CONSUMER_KEY) {
     tweet = require('./tweet.js')
   }
 
 
   const Datastore = require('nedb')
-   var db = new Datastore({ filename: path.join(__dirname, '/db/saved_sketches'), autoload: true})
+  var db = new Datastore({ filename: path.join(__dirname, '/db/saved_sketches'), autoload: true })
 
- //console.log('dir is', __dirname)
-    var sketches = []
+  //console.log('dir is', __dirname)
+  var sketches = []
 
-    db.count({}, function (err, count) {
-      // console.log("There are " + count + " entries in the database");
-      if(err) console.log("There's a problem with the database: ", err);
-      else if(count<=0){ // empty database so needs populating
-        // default users inserted in the database
-        db.insert(sketches, function (err, testAdded) {
-          if(err) console.log("There's a problem with the database: ", err);
-          else if(testAdded) console.log("Default users inserted in the database");
-        });
+  db.count({}, function (err, count) {
+    // console.log("There are " + count + " entries in the database");
+    if (err) console.log("There's a problem with the database: ", err);
+    else if (count <= 0) { // empty database so needs populating
+      // default users inserted in the database
+      db.insert(sketches, function (err, testAdded) {
+        if (err) console.log("There's a problem with the database: ", err);
+        else if (testAdded) console.log("Default users inserted in the database");
+      });
+    }
+  });
+
+  app.get('/sketches', function (request, response) {
+    db.find({}, function (err, entries) {
+      if (err) {
+        console.log('problem with db', err)
+      } else {
+        var res = entries.map((entry) => {
+          entry.sketch_id = entry._id
+          return entry
+        })
+        response.send(entries)
       }
-    });
-
-    app.get('/sketches', function (request, response) {
-      db.find({}, function (err, entries){
-        if (err) {
-          console.log('problem with db', err)
-        } else {
-          var res = entries.map((entry) => {
-            entry.sketch_id = entry._id
-            return entry
-          })
-          response.send(entries)
-        }
-      })
     })
+  })
 
-    app.get('/sketchById', function (request, response) {
-      db.find({_id: request.query.sketch_id}, function (err, entries){
-        if (err) {
-          console.log('problem with db', err)
-        } else {
-          var res = entries.map((entry) => {
-            entry.sketch_id = entry._id
-            return entry
-          })
-          response.send(entries)
-        }
-      })
+  app.get('/sketchById', function (request, response) {
+    db.find({ _id: request.query.sketch_id }, function (err, entries) {
+      if (err) {
+        console.log('problem with db', err)
+      } else {
+        var res = entries.map((entry) => {
+          entry.sketch_id = entry._id
+          return entry
+        })
+        response.send(entries)
+      }
     })
+  })
 
-    app.post('/sketch', function (request, response) {
+  app.post('/sketch', function (request, response) {
     //  console.log('post sketch', request.query)
-      db.insert({
-        "code": request.query.code,
-        "parent": request.query.parent,
-        "date": new Date()
-      }, function (err, sketchAdded) {
-        if (err) {
-          console.log('error adding', err)
-          response.sendStatus(500)
-        } else {
+    db.insert({
+      "code": request.query.code,
+      "parent": request.query.parent,
+      "date": new Date()
+    }, function (err, sketchAdded) {
+      if (err) {
+        console.log('error adding', err)
+        response.sendStatus(500)
+      } else {
         //  console.log('ADDED', sketchAdded)
-          response.send(sketchAdded._id)
-        }
-      })
+        response.send(sketchAdded._id)
+      }
     })
+  })
 
-    // app.post('/image', function (request, response) {
-    //   console.log('post sketch', request.query)
-    // })
+  // app.post('/image', function (request, response) {
+  //   console.log('post sketch', request.query)
+  // })
 
 
-    //const storage = multer.memoryStorage();
+  //const storage = multer.memoryStorage();
 
-    // var storage = multer.diskStorage({
-    //   destination: function (req, file, cb) {
-    //    cb(null, path.join(__dirname + '/uploads/'))
-    //    },
-    //    filename: function (req, file, cb) {
-    //      cb(null, file.originalname + '.png')
-    //    }
-    // })
+  // var storage = multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //    cb(null, path.join(__dirname + '/uploads/'))
+  //    },
+  //    filename: function (req, file, cb) {
+  //      cb(null, file.originalname + '.png')
+  //    }
+  // })
 
   //  console.log('envv', process.env)
   app.post('/image', (req, res) => {
     // let data = ÷\ß\req.body;
-    res.send('gallery is currently disabled, stay tuned for updates');
-})
-    if(process.env.CONSUMER_KEY) {
+    //res.send('gallery is currently disabled, stay tuned for updates');
+    res.send('twitter gallery is down, but sketch code has been saved to hydra database')
+  })
+  if (process.env.CONSUMER_KEY) {
     //  const upload = multer({ storage: storage });
     //  app.post("/image", upload.single('previewImage'), (req, res) => {
     //   //  const updatedNameString = req.query.name.replace('@', '{@}')
@@ -123,54 +124,54 @@ module.exports = (app) => {
     //   //        }, function (err, numReplaced) {});
     //   //      }
     //   //    })
-       
+
     //   //  })
-    
+
     //  });
 
     function findParentTweet(sketch_id, callback) {
-      db.find({_id: sketch_id}, function (err, entries){
-       if(err){
-         callback(err, null)
-       } else {
-         if(entries.length > 0){
-           if(entries[0].parent) {
-               db.find({_id: entries[0].parent}, function (err, entries){
-                 if(err){
-                   callback(err)
-                 } else {
-                   if(entries.length > 0){
-                     if(entries[0].tweet_id) {
-                       callback(null, entries[0].tweet_id)
-                     } else {
-                       callback(null, null)
-                     }
-                   } else {
-                     callback(null, null)
-                   }
-                 }
-               })
-           } else {
-             callback(null, null)
-           }
-         } else {
-           callback(null, null)
-         }
-       }
-     })
+      db.find({ _id: sketch_id }, function (err, entries) {
+        if (err) {
+          callback(err, null)
+        } else {
+          if (entries.length > 0) {
+            if (entries[0].parent) {
+              db.find({ _id: entries[0].parent }, function (err, entries) {
+                if (err) {
+                  callback(err)
+                } else {
+                  if (entries.length > 0) {
+                    if (entries[0].tweet_id) {
+                      callback(null, entries[0].tweet_id)
+                    } else {
+                      callback(null, null)
+                    }
+                  } else {
+                    callback(null, null)
+                  }
+                }
+              })
+            } else {
+              callback(null, null)
+            }
+          } else {
+            callback(null, null)
+          }
+        }
+      })
     }
-    }
+  }
 
-     function saveFile(body, fileName) {
-       const file = fs.createWriteStream(fileName)
-       request(body).pipe(file).on('close', err => {
-         if (err) {
-           console.log(err)
-         } else {
-          //  console.log('Media saved!')
-           const descriptionText = body.title
-          // uploadMedia(descriptionText, fileName)
-         }
-       })
-     }
+  function saveFile(body, fileName) {
+    const file = fs.createWriteStream(fileName)
+    request(body).pipe(file).on('close', err => {
+      if (err) {
+        console.log(err)
+      } else {
+        //  console.log('Media saved!')
+        const descriptionText = body.title
+        // uploadMedia(descriptionText, fileName)
+      }
+    })
+  }
 }
